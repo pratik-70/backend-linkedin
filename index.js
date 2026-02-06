1e9ae9b7c218bd7a1987a41da3670f006186103a
pratik708 <70812345pratik@gmail.com.com>
2026-02-06 15:02:20 +0530

    vercel added

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ app.get("/", (req, res) => {
 
 // server-> http://localhost:4000
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port ", process.env.PORT)
-})
+module.exports = app;
+
+// app.listen(process.env.PORT, () => {
+//     console.log("Server is running on port ", process.env.PORT)
+// })
